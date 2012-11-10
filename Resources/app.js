d58c24377aefc18de115ee02cc2686ce8e9008e9
Nick <nrellihan@sportinginnovations.com>
2012-11-10 18:00:58 +0000

    Navigation

--- Resources/app.js
+++ Resources/app.js
@@ -1,2 +1,17 @@
-login = require("js/login");
-login.show();
+
+try {
+  tabs = require("js/TabBar");
+  list = require("js/OffersList");
+  listView = new list();
+  tabs.add(listView.view);
+  offer = require("js/OfferView");
+  offerView = new offer();
+  tabs.add(offerView.view);
+  confirm = require("js/ConfirmationView");
+  confirmView = new confirm();
+  tabs.add(confirmView.view);
+  tabs.open();
+}
+catch (exception) {
+  Ti.API.info(exception)
+}
